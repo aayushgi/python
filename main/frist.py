04892e9c7b6527652a0587d5bb6e8279ea76ddc4
Ayush saxena <155671320+aayushgi@users.noreply.github.com>
2025-09-04 10:28:11 +0530

    Add files via upload

--- main/frist.py
+++ main/frist.py
@@ -1,1 +1,42 @@
 print("hellow")
+#comments in python
+#variable 
+a="ayush"
+#print(a)
+#user input
+name=input("enter your name ")
+#print(name)
+#age=int(input("enter your age hear: "))
+#print(age)
+#exp1=eval(input("enter any equation to fint the solution"))
+#print(exp1)
+# print("name\n" \
+# "age\n" \
+# "address"
+# )
+# x=45
+# y=89
+# temp=x
+# x=y
+# y=temp
+# print(x)
+# print(y)
+
+# x=12.4
+# print(type(x))
+# x=int(x)
+# print(type(x))
+# print(x)
+name=input("enter the name of the student: ")
+grade=input("enter the grade of the student: " )
+age=input("input the age of the student: ")
+email=input("enter the email address of the student: ")
+phone_number=input("enter the phone number of the student: ")
+
+print("student identity card is here")
+print(name)
+print(grade)
+print(age)
+print(email)
+print(phone_number)
+
